bad86d6e9cf6f140d2120370d181c9dc4cec1ec5
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-02-25 13:02:41 +0600

    card header reusable

--- src/Components/PremiseV2/Card/PremiseCardV2.jsx
+++ src/Components/PremiseV2/Card/PremiseCardV2.jsx
@@ -1,16 +1,8 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { FaEllipsisV } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 
 import backgroundImg from "../../../img/Icons/download.jpg";
-import mailCart from "../../../img/Icons/mailCart.png";
-import mailCartQ from "../../../img/Icons/mailCartQ.png";
 // import transCartQ from "../../../img/Icons/transCartQ.png";
-import msgIcon from "../../../img/Icons/msgIcon.png";
-import sourceIcn from "../../../img/Icons/sourceIcn.png";
-import transCartQ from "../../../img/Icons/transCartQ.png";
-import translateCart from "../../../img/Icons/translateCart.png";
-import transReqQ from "../../../img/Icons/transReqQ.png";
 import userImg from "../../../img/Icons/userImg.png";
 
 import { fetchUserAccess, MyContext } from "../../../App";
@@ -26,7 +18,6 @@ import { setPremise } from "../../../app/Slices/premiseSlice";
 import CharacterEditablePop from "../../Premisepool/Character/CharacterEditablePop";
 import CommentPremise from "../../Premisepool/CommentPremise";
 import AddPremise2 from "../../Premisepool/Components/AddPremise2";
-import HideOptionPop from "../../Premisepool/Components/HideOptionPop";
 import LikePremise from "../../Premisepool/LikePremise";
 import OwnerMail from "../../Premisepool/OwnerMail";
 import Popup from "../../Premisepool/Popup";
@@ -46,7 +37,7 @@ import ReqTranslationPop from "../Popups/ReqTranslationPop";
 import SaleRequestedOwner from "../Popups/SaleRequested_Owner";
 import TransInOtherLang from "../Popups/TransInOtherLang.pop";
 import ViewTranslationPop from "../Popups/ViewTranslation.pop";
-import { handlePremiseOpenNewTab } from "../utilityFuncitons/functions";
+import CardHeadOptions from "./CardHeadOptions";
 import PremiseBadge from "./PremiseBadge";
 
 const PremiseCardV2 = ({
@@ -418,301 +409,63 @@ const PremiseCardV2 = ({
                     <p className="notranslate text-[#252525] text-[12px]">
                       {currentProjectName?.slice(0, 20)}
                     </p>
-                  ) : <br></br>
-                  }
+                  ) : (
+                    <br></br>
+                  )}
                 </div>
               </div>
             </div>
           </a>
         </div>
 
-        <div>
-          {" "}
-          {premiseOwner?.id === user ? (
-            <div className="flex gap-[3px] items-center mt-[-13px] mr-[2px] relative ">
-              {translation_request_count > 0 && (
-                <div className="relative">
-                  <span className="absolute top-[-17px] right-0 text-[12px] font-[700] text-[#252525]">
-                    {translation_request_count > 1 && (
-                      <>{translation_request_count}</>
-                    )}
-                  </span>
-                  <img
-                    data-te-toggle="tooltip"
-                    title="Translation Requests"
-                    src={transReqQ}
-                    className="w-8 h-8 cursor-pointer"
-                    alt=""
-                    onClick={() => setViewTrnRequests(id)}
-                  />
-                </div>
-              )}
-              {is_translated_languages > 0 && (
-                <img
-                  data-te-toggle="tooltip"
-                  title="Translated Languages"
-                  src={translateCart}
-                  className="w-8 h-8 cursor-pointer"
-                  alt=""
-                  onClick={() => handleViewTransaction(id)}
-                />
-              )}
-
-              {premise_source_id && (
-                <img
-                  data-te-toggle="tooltip"
-                  title="View Source"
-                  src={sourceIcn}
-                  className="w-8 h-8 cursor-pointer"
-                  alt=""
-                  onClick={() => handlePremiseOpenNewTab(premise_source_id)}
-                />
-              )}
-              {sale_request_count > 0 && (
-                <div className="">
-                  <img
-                    data-te-toggle="tooltip"
-                    title="Sale Requested"
-                    src={mailCartQ}
-                    className="w-9 h-9 cursor-pointer"
-                    alt=""
-                    onClick={() => setViewSaleRequests(true)}
-                  />
-                </div>
-              )}
-
-              <FaEllipsisV
-                onMouseDown={(e) => {
-                  e.stopPropagation();
-                  setOpenDotMenu((prevIndex) =>
-                    prevIndex === index ? null : index
-                  );
-                }}
-                className="w-5 h-5 cursor-pointer"
-              />
-              {openDotMenu === index && (
-                <div
-                  ref={dotPopupRef}
-                  className="absolute flex flex-col w-[197px] font-[400] text-[#616161] px-3 bg-[#fafafa] rounded-[8px] shadow-md border border-[#eaeaea] top-[25px] right-[3px] py-[8px]  z-10"
-                >
-                  <button
-                    onClick={handleVisibility}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] break-none text-left hover:text-[#33B0CA] text-[#252525]">
-                      {" "}
-                      Visibility Settings
-                    </p>{" "}
-                  </button>
-
-                  <button
-                    onClick={() => {
-                      setOpenTransOtherPop(!openTransOtherPop);
-                      setOpenDotMenu(null);
-                    }}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] break-none text-left hover:text-[#33B0CA] text-[#252525]">
-                      {" "}
-                      Copy in new Language
-                    </p>{" "}
-                  </button>
-
-                  <button
-                    onClick={handleMonetizing}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] break-none text-left hover:text-[#33B0CA] text-[#252525]">
-                      {" "}
-                      Monetizing Preferences
-                    </p>{" "}
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleViewTransaction(id);
-                    }}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] break-none text-left hover:text-[#33B0CA] text-[#252525]">
-                      {" "}
-                      View Translations
-                    </p>{" "}
-                  </button>
-
-                  <button
-                    onClick={() => {
-                      handleDelete(id);
-                      setOpenDotMenu(null);
-                    }}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] text-left hover:text-[#33B0CA] break-none text-[#252525]">
-                      {" "}
-                      Delete Premise
-                    </p>{" "}
-                  </button>
-                  <button
-                    onClick={() => {
-                      setOpenCharacterChart(project_id);
-                      setOpenDotMenu(null);
-                    }}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] text-left hover:text-[#33B0CA] break-none text-[#252525]">
-                      {" "}
-                      Characters and Roles
-                    </p>{" "}
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleOpenSp();
-                      setOpenDotMenu(null);
-                    }}
-                    className="cursor-pointer  w-full"
-                  >
-                    <p className="text-[14px] w-full font-[500] text-left hover:text-[#33B0CA] break-none text-[#252525]">
-                      {" "}
-                      Open <span className="scriptpad-m">Script Pad</span>
-                    </p>{" "}
-                  </button>
-
-                  {/* */}
-                </div>
-              )}
-              {/* <FaEllipsisV
-                onClick={() => setOpenHidePop(!openHidePop)}
-                className="w-5 h-5 cursor-pointer"
-              /> */}
-              {openHidePop?.msg === "ShowBecomePrivilege" ? (
-                <NoAccessPopUp
-                  noAccessPopup={openHidePop}
-                  setNoAccessPopup={setOpenHidePop}
-                />
-              ) : openHidePop?.msg === "LB" ||
-                openHidePop?.msg === "ShowBuyPackage_and_Allacarte" ? (
-                <NoAccessLbPopUp
-                  noAccessLbPopup={openHidePop}
-                  setNoAccessPopup={setOpenHidePop}
-                  service="PP_Private"
-                />
-              ) : (
-                openHidePop === "Yes" && (
-                  <HideOptionPop
-                    setOpenHidePop={setOpenHidePop}
-                    id={id}
-                    refetch={refetch}
-                    user={user}
-                    filter_flag={filter_flag}
-                    comment_filter_flag={comment_filter_flag}
-                    visible_to={visible_to}
-                  />
-                )
-              )}
-            </div>
-          ) : (
-            <div className="flex gap-[3px] items-center  mr-[2px] relative ">
-              {available_for_translation ? (
-                <img
-                  data-te-toggle="tooltip"
-                  title="Available for Translation"
-                  src={translateCart}
-                  className="w-8 h-8 mt-[-13px] cursor-pointer"
-                  alt=""
-                  onClick={() => {
-                    setOpenAvailableForTranslationPop(
-                      !openAvailableForTranslationPop
-                    );
-                    // setOpenDotMenu(null);
-                  }}
-                />
-              ) : (
-                <div className="relative">
-                  <img
-                    data-te-toggle="tooltip"
-                    title="Send Translation Request"
-                    src={transCartQ}
-                    className="w-8 h-8 mt-[-13px] cursor-pointer"
-                    alt=""
-                    onClick={() => checkAllowance(setTranslationRequestPop, id)}
-                  />
-                </div>
-              )}
-
-              {available_for_sale ? (
-                <img
-                  data-te-toggle="tooltip"
-                  title="Available for Sale"
-                  src={mailCart}
-                  className="w-8 h-8 mt-[-13px] cursor-pointer"
-                  onClick={() => handleSale(id)}
-                  alt="for sale"
-                />
-              ) : (
-                <>
-                  {
-                    <button
-                      className={`${is_requested_for_sale
-                          ? "cursor-default"
-                          : " cursor-pointer"
-                        }`}
-                      data-te-toggle="tooltip"
-                      title="Send Sale Request"
-                      disabled={is_requested_for_sale}
-                      onClick={() => {
-                        checkAllowance(setSaleRequestPop, id);
-                        refetch();
-                      }}
-                    >
-                      <img
-                        src={mailCartQ}
-                        className={`w-9 h-9 mt-[-13px]`}
-                        alt="send sale request"
-                      />
-                    </button>
-                  }
-                </>
-              )}
-              {premise_source_id && (
-                <img
-                  data-te-toggle="tooltip"
-                  title="View Source"
-                  src={sourceIcn}
-                  className="w-8 h-8 mt-[-13px] cursor-pointer"
-                  alt=""
-                  onClick={() => handlePremiseOpenNewTab(premise_source_id)}
-                />
-              )}
-              <img
-                data-te-toggle="tooltip"
-                title="Send Message"
-                src={msgIcon}
-                className="w-8 h-8 mt-[-13px] cursor-pointer"
-                alt=""
-                onClick={handleUserMail}
-              />
-            </div>
-          )}
-        </div>
+        <CardHeadOptions
+          p={p}
+          owner={owner}
+          index={index}
+          refetch={refetch}
+          viewTrnRequests={viewTrnRequests}
+          setViewTrnRequests={setViewTrnRequests}
+          viewTransactionPId={viewTransactionPId}
+          setViewTransactionPId={setViewTransactionPId}
+          setViewSaleRequests={setViewSaleRequests}
+          openTransOtherPop={openTransOtherPop}
+          setOpenTransOtherPop={setOpenTransOtherPop}
+          handleDelete={handleDelete}
+          setOpenCharacterChart={setOpenCharacterChart}
+          openViewTranslationsPop={openViewTranslationsPop}
+          openAvailableForTranslationPop={openAvailableForTranslationPop}
+          setOpenAvailableForTranslationPop={setOpenAvailableForTranslationPop}
+          setOpenViewTranslationsPop={setOpenViewTranslationsPop}
+          setOpenMonetizingPreferencesPop={setOpenMonetizingPreferencesPop}
+          setNoAccessLbPopUp={setNoAccessLbPopUp}
+          setUserMail={setUserMail}
+          setSaleId={setSaleId}
+          setViewSale={setViewSale}
+          setSaleRequestPop={setSaleRequestPop}
+          setTranslationRequestPop={setTranslationRequestPop}
+          isProjectLocked={isProjectLocked}
+        />
       </div>
       {/* middle div */}
       <div className="bg-[#FAFAFA] h-[189px] border !border-[#f8f8f8] relative">
         <div
           className={` w-[93%] bg-[#FAFAFA]  h-[189px] rounded-[8px]  mx-auto   border border-[#eaeaea] relative
-          ${imageLoaded && bg_img
+          ${
+            imageLoaded && bg_img
               ? "premiseBg-loaded rounded-[8px]"
               : "rounded-[8px]"
-            }`}
+          }`}
           style={
             bg_img
               ? {
-                backgroundImage: `url(${bg_img})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                width: "92%",
-                borderRadius: "8px",
-                // boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
-              }
+                  backgroundImage: `url(${bg_img})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "92%",
+                  borderRadius: "8px",
+                  // boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+                }
               : { backgroundColor: bg_color }
           }
           onLoad={() => setImageLoaded(true)}
@@ -886,7 +639,6 @@ const PremiseCardV2 = ({
           source_language={source_language}
           project_id={project_id}
           refetch={refetch}
-
         />
       )}
       {openViewTranslationsPop && (
@@ -901,7 +653,6 @@ const PremiseCardV2 = ({
             refetch,
             viewText,
           }}
-
         />
       )}
       {openMonetizingPreferencesPop?.msg == "ShowBecomePrivilege" ? (
@@ -918,7 +669,7 @@ const PremiseCardV2 = ({
         />
       ) : (
         openMonetizingPreferencesPop == "Yes" && (
-          <MonetizePreferencePop 
+          <MonetizePreferencePop
             popClose={setOpenMonetizingPreferencesPop}
             id={id}
             user={user}
