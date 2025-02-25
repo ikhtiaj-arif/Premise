f1d85ddcd767bca4db37f76b6150333da95d8892
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-02-25 10:57:13 +0600

    merge

--- src/Components/PremiseV2/Popups/EachTranslateeCard.jsx
+++ src/Components/PremiseV2/Popups/EachTranslateeCard.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import {
-  useGetOnePremiseQuery,
-  useGetUserByUserIdQuery,
-} from "../../../app/EndPoints/premisePoolApi";
+import { useGetOnePremiseQuery, useGetUserByUserIdQuery } from "../../../app/EndPoints/premisePoolApi";
 import { getLanguageName } from "../utilityFuncitons/functions";
+import Popup from "../../Premisepool/Popup";
 
 
 const EachTranslateeCard = ({
@@ -28,7 +26,7 @@ const EachTranslateeCard = ({
   } = useGetOnePremiseQuery(transaction?.translatedToPremiseID);
 
   const lang = getLanguageName(transaction?.translatedIn);
-  const [popup, setPopUp] = useState(false);
+  const [popup, setPopUp] = useState(false)
 
   const popClose = () => {
     setPopUp(false)
@@ -37,39 +35,44 @@ const EachTranslateeCard = ({
 
   const data = premiseData
     ? {
-        stylings: premiseData?.text?.includes("+")
-          ? JSON.parse(premiseData?.text?.split("+")[0])
-          : {}, // Default to an empty object if `text` is undefined or improperly formatted
-        bg_color: premiseData?.bg_color || "",
-        bg_img: premiseData?.bg_img || "",
-        comments: premiseData?.comments || [],
-        created_at: premiseData?.created_at || "",
-        likes: premiseData?.likes || 0,
-        id: premiseData?.id || "",
-        source_language: premiseData?.source_language || "",
-        updated_at: premiseData?.updated_at || "",
-        dText: premiseData?.text?.includes("+")
-          ? premiseData?.text?.split("+")[1]
-          : "",
-        project_id: premiseData?.pro_uuid || "",
-        m_value: premiseData?.m_value || "",
-      }
+      stylings: premiseData?.text?.includes("+")
+        ? JSON.parse(premiseData?.text?.split("+")[0])
+        : {}, // Default to an empty object if `text` is undefined or improperly formatted
+      bg_color: premiseData?.bg_color || "",
+      bg_img: premiseData?.bg_img || "",
+      comments: premiseData?.comments || [],
+      created_at: premiseData?.created_at || "",
+      likes: premiseData?.likes || 0,
+      id: premiseData?.id || "",
+      source_language: premiseData?.source_language || "",
+      updated_at: premiseData?.updated_at || "",
+      dText: premiseData?.text?.includes("+") ? premiseData?.text?.split("+")[1] : "",
+      project_id: premiseData?.pro_uuid || "",
+      m_value: premiseData?.m_value || "",
+    }
     : {};
+
+
+ 
 
   return (
     <React.Fragment>
+
       {
-        // popup && <Popup
-        //   refetch={premiseRefetch}
-        //   {...{
-        //     popClose,
-        //     handleVisibility,
-        //     handleMonetizing,
-        //     viewText,
-        //   }}
-        //   data={data}
-        // />
+        popup && <Popup
+          refetch={premiseRefetch}
+
+          {...{
+            popClose,
+            handleVisibility,
+            handleMonetizing,
+            viewText,
+          }}
+          data={data}
+
+        />
       }
+
 
       {/* Translated In */}
       <div className="flex flex-col col-span-3">
@@ -91,8 +94,7 @@ const EachTranslateeCard = ({
         <div className="font-[400] text-[14px] leading-[21px] text-[#616161] text-center my-[9px]">
           {/* {allowedUserData?.firstName} {allowedUserData?.lastName}
            */}
-          {transaction?.translationAllowedBy?.first_name}{" "}
-          {transaction?.translationAllowedBy?.last_name}
+          {transaction?.translationAllowedBy?.first_name}{" "}{transaction?.translationAllowedBy?.last_name}
         </div>
       </div>
 
@@ -104,8 +106,7 @@ const EachTranslateeCard = ({
         <div className="h-[2px] mt-[4px] w-[86%] mx-auto bg-[#a1a1a1]" />
         <div className="font-[400] text-[14px] leading-[21px] text-[#616161] text-center my-[9px]">
           {/* {userData?.firstName} {userData?.lastName} */}
-          {transaction?.translatedFor?.first_name}{" "}
-          {transaction?.translatedFor?.last_name}
+          {transaction?.translatedFor?.first_name}{" "}{transaction?.translatedFor?.last_name}
         </div>
       </div>
 
