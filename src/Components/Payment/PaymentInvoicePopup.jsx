3936d95dcf22c22d148e1dc8e33c8b4fbfa6f944
Shakiba <shakibaalam092@gmail.com>
2025-03-05 13:33:20 +0600

    payment change

--- src/Components/Payment/PaymentInvoicePopup.jsx
+++ src/Components/Payment/PaymentInvoicePopup.jsx
@@ -73,6 +73,7 @@ const PaymentInvoicePopup = ({
         data.razorpay_payment_id = response?.razorpay_payment_id;
         data.razorpay_signature = response?.razorpay_signature;
         data.transaction_id = payInfo?.transaction_id;
+        data.premise_id = payInfo?.premise_id;
       }
       const creditToDebit = sessionStorage.getItem("creditToDebit");
       if (creditToDebit) {
