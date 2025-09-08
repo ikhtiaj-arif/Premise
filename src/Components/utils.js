0f0db6d34d29aff942059bc6cd88523b40586d2e
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-09-08 17:38:29 +0600

    latest

--- src/Components/utils.js
+++ src/Components/utils.js
@@ -15,8 +15,8 @@
 // const URL = window.location.origin;
 // const baseURL = window.location.origin;
 
-const URL = "http://1.6.141.108";
-const baseURL = "http://1.6.141.108";
+// const URL = "http://1.6.141.108";
+// const baseURL = "http://1.6.141.108";
 
 // const URL = "https://taj4o.mynextfilm.in";
 // const baseURL = "https://taj4o.mynextfilm.in";
